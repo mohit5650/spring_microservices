f64221e8f08dfbf8fff3840b2f1e87079a6e7875
mohit5650 <46513332+mohit5650@users.noreply.github.com>
2021-06-10 22:36:06 +0530

    Add files via upload

---
+++ zuulapigatewaydemo/src/main/java/com/brainmentors/apps/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.brainmentors.apps;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+	public void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/contact").permitAll();
+		
+	}
+
+}
